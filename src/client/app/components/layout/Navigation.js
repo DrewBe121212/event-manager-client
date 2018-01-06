3539a5b87d8a462f0948b63431f2aa196904d204
Drew Dulgar <drewdulgar@Event-Technologiess-MacBook-Pro.local>
2018-01-06 12:21:09 -0500

    Add title and version number to menu header

--- src/client/app/components/layout/Navigation.js
+++ src/client/app/components/layout/Navigation.js
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
 
+import Typography from 'material-ui/Typography';
 import Drawer from 'material-ui/Drawer';
+import {MenuList, MenuItem} from 'material-ui/Menu';
+import { ListItemIcon, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
 import ChevronLeftIcon from 'material-ui-icons/ChevronLeft';
+import ScheduleIcon from 'material-ui-icons/Schedule';
+
+import {config} from 'config';
 
 const styles = (theme) => ({
   drawerPaper: {
@@ -19,6 +25,9 @@ const styles = (theme) => ({
     justifyContent: 'flex-end',
     padding: '0 8px',
     ...theme.mixins.toolbar
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4
   }
 });
 
@@ -49,11 +58,27 @@ class NavigationComponent extends React.Component {
       <Drawer type="persistent" open={this.props.drawer.open} classes={{paper: classes.drawerPaper}}>
         <div className={classes.drawerInner}>
           <div className={classes.drawerHeader}>
+            <div>
+              <div>
+                <Typography type="title" gutterBottom>{config.APPLICATION.NAME}</Typography>
+              </div>
+              <div>
+                <Typography type="caption" align="right">Version {config.APPLICATION.VERSION}</Typography>
+              </div>
+            </div>
             <IconButton onClick={this.closeDrawer}>
               <ChevronLeftIcon />
             </IconButton>
           </div>
           <Divider />
+          <MenuList>
+            <MenuItem>
+              <ListItemIcon>
+                <ScheduleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Daily Schedule" />
+            </MenuItem>
+          </MenuList>
         </div>
       </Drawer>
     );
